26b2db6d1465d63f781260cc64d90ced3f887e5e
Andrew Neidley <neidz44@gmail.com>
2018-05-16 21:42:52 -0500

    create post POST route and validation

--- models/Post.js
+++ models/Post.js
@@ -1,9 +1,9 @@
-const mongoose, require('mongoose');
+const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const PostSchema = new Schema({
   user: {
-    type: Schema.types.ObjectId,
+    type: Schema.Types.ObjectId,
     ref: 'users'
   },
   text: {
@@ -19,7 +19,7 @@ const PostSchema = new Schema({
   likes: [
     {
       user: {
-        type: Schema.Types.ObjectId
+        type: Schema.Types.ObjectId,
         ref: 'users'
       }
     }
@@ -27,7 +27,7 @@ const PostSchema = new Schema({
   comments: [
     {
       user: {
-        type: Schema.Types.ObjectId
+        type: Schema.Types.ObjectId,
         ref: 'users'
       },
       text: {
@@ -51,3 +51,5 @@ const PostSchema = new Schema({
     default: Date.now
   }
 });
+
+module.exports = Post = mongoose.model('post', PostSchema);
